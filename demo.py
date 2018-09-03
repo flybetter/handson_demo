19c06c211fb0af36cf340f6014b36bc531e63ab8
flybetter <flybetter@163.com>
2018-09-03 20:31:40 +0800

    Initial commit

---
+++ demo.py
@@ -0,0 +1,51 @@
+import os
+import tarfile
+from six.moves import urllib
+
+DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml/master/"
+HOUSING_PATH = "datasets/housing"
+HOUSING_URL = DOWNLOAD_ROOT + HOUSING_PATH + "/housing.tgz"
+
+
+def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
+    if not os.path.isdir(housing_path):
+        os.makedirs(housing_path)
+    tgz_path = os.path.join(housing_path, "housing.tgz")
+    urllib.request.urlretrieve(housing_url, tgz_path)
+    housing_tgz = tarfile.open(tgz_path)
+    housing_tgz.extractall(path=housing_path)
+    housing_tgz.close()
+
+
+if __name__ == '__main__':
+    fetch_housing_data()
+
+
+
+
+import hashlib
+def test_set_check(identifier,test_ratio,hash):
+    return hash(np.int64(identifier)).digest()[-1]<256*test_ratio
+
+def split_train_test_by_id(data,test_ratio,id_column,hash=hashlib.md5):
+    ids=data[id_column]
+    in_test_set=ids.apply(lambda id_:test_set_check(id_,test_ratio,hash))
+    return data.iloc[~in_test_set],data.loc[in_test_set]
+
+housing_with_id = housing.reset_index()   # adds an `index` column
+train_set, test_set = split_train_test_by_id(housing_with_id, 0.2, "index")
+
+
+import hashlib
+
+def test_set_check(identifier, test_ratio, hash):
+    return hash(np.int64(identifier)).digest()[-1] < 256 * test_ratio
+
+def split_train_test_by_id(data, test_ratio, id_column, hash=hashlib.md5):
+    ids = data[id_column]
+    in_test_set = ids.apply(lambda id_: test_set_check(id_, test_ratio, hash))
+    return data.loc[~in_test_set], data.loc[in_test_set]
+
+housing_with_id = housing.reset_index()   # adds an `index` column
+train_set, test_set = split_train_test_by_id(housing_with_id, 0.2, "index")
+
