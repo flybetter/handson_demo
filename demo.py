4daac97cfbd8c02f1a284425fe6cde41263f7ded
flybetter <flybetter@163.com>
2018-09-05 16:29:59 +0800

    now i finish the chapter 2 regression.

--- demo.py
+++ demo.py
@@ -49,3 +49,4 @@ def split_train_test_by_id(data, test_ratio, id_column, hash=hashlib.md5):
 housing_with_id = housing.reset_index()   # adds an `index` column
 train_set, test_set = split_train_test_by_id(housing_with_id, 0.2, "index")
 
+
